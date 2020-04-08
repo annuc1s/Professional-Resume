03c866041304f22ac0bff6fc385d46461a0a9da4
annuc1s <annuciis121@gmail.com>
2020-04-08 16:06:22 +0000

    script added to clear contact form after submission

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,3 +1,4 @@
+//active contact form linked to the developers gmail
 function sendMail(contactForm) {
     emailjs.send("gmail", "online_portfolio", {
         "from_name": contactForm.name.value,
@@ -5,13 +6,18 @@ function sendMail(contactForm) {
         "subject": contactForm.subject.value,
         "work_together": contactForm.message.value
     })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("ERROR", error);
-        }
-    )
-        return false;
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("ERROR", error);
+            }
+        )
+    return false;
 };
+
+//form resets after submission
+$(".reset").click(function () {
+    $(this).closest('form').find("input[type=text], textarea").val("");
+});
